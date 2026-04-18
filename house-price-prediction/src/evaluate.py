7618b9f684a7a084743b347cde4c2688390e3027
Rahul Singh <rahulxsingh2005@gmail.com>
2026-04-18 14:58:21 +0530

    Add script for train CV, rmse and r2_score.

--- house-price-prediction/src/evaluate.py
+++ house-price-prediction/src/evaluate.py
@@ -4,3 +4,15 @@ from sklearn.model_selection import cross_val_score
 
 def evaluate(model, X_train, y_train_log, X_test, y_test):
     
+    # Train score
+    y_train_pred = model.predict(X_train)
+    
+    # CV score on train
+    cv_scores = cross_val_score(model, X_train, y_train_log, cv=5, scoring='r2')
+    mean_cv = cv_scores.mean()
+    
+    # Train metrics
+    train_rmse = root_mean_squared_error(y_train_log, y_train_pred)
+    train_r2 = root_mean_squared_error(y_train_log, y_train_pred)
+
+    
