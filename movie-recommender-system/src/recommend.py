f7797eab7c531598ff5baf4d22f8ee56190acefb
Rahul Singh <rahulxsingh2005@gmail.com>
2026-05-09 23:34:16 +0530

    feat: implement recommendation engine

---
+++ movie-recommender-system/src/recommend.py
@@ -0,0 +1,18 @@
+from vectorize import vectorize
+
+def recommend_movie(movie):
+    # Get the movies and similarity scores
+    movies, similarity = vectorize()
+    
+    # Fetch the movie index
+    movie_index = movies[movies['title'] == movie].index[0]
+    
+    # Get the similarity array for movie
+    distances = similarity[movie_index]
+    
+    # Calculate 5 similar movies
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
+    
+    # Print the movies
+    for i in  movie_list:
+        print(movies.iloc[i[0]].title)
