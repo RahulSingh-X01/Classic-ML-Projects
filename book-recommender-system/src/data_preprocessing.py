410b402f9c50e6da41a27bb7474d8b2496d4ed7f
Rahul Singh <rahulxsingh2005@gmail.com>
2026-06-19 07:34:01 +0530

    feat: add collaborative filtering preprocessing pipeline

--- book-recommender-system/src/data_preprocessing.py
+++ book-recommender-system/src/data_preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 def load_data():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -53,5 +54,29 @@ def popular_movies(top_n=50, min_ratings=250):
     popular_movies = popular_movies[['Book-Title', 'Book-Author', 'Image-URL-M', 'num-rating', 'avg-rating']]
     
     return popular_movies
+
+def collaborative_model(min_book_rating=50, min_user_rating=200):
+    books_data, user_data, ratings_data = load_data()
     
+    # Merge ratings and books dataset on 'ISBN' column
+    ratings_with_num = ratings_data.merge(books_data, on='ISBN')
     
+    # Getting users who have given minimum 200 ratings
+    users_with_high_ratings = ratings_with_num.groupby('User-ID').count()['Book-Rating'] > min_user_rating
+    x = users_with_high_ratings[users_with_high_ratings].index
+    
+    # Getting books with minimum 50 ratings
+    books_with_high_ratings = ratings_with_num.groupby('Book-Title').count()['Book-Rating'] > min_book_rating
+    y = books_with_high_ratings[books_with_high_ratings].index
+    
+    # Filter data according to selected users and books
+    filtered_rating = ratings_with_num[ratings_with_num['User-ID'].isin(x)]
+    final_ratings = filtered_rating[filtered_rating['Book-Title'].isin(y)]
+    
+    # Make a new dataframe with book-titles as indices
+    movie_data = final_ratings.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating').fillna(0)
+    
+    # Get similarity score of each movie with every other movie accoridng to cosine similarity
+    similarity_score = cosine_similarity(movie_data)
+    
+    return movie_data, similarity_score 
